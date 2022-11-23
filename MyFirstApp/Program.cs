b60aeac4602338c4264e6ea9413406b283377a09
Sigrit <sigrit.sults@tktk.ee>
2022-11-23 19:16:15 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿//This is a comment to my code
+
+//rakendus küsib kasutaja nime
+//ja siis tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:");
+//string on sõne
+string userName = Console.ReadLine();
+
+Console.WriteLine("Hello, " + userName +"!");
+//string interpolation
+
+Console.WriteLine($"Hello, {userName}!");
